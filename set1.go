0ad9031f0e7c5e5b315a5cf81e7ceffbe6348b81
shxdow <dysonfrost@protonmail.ch>
2018-07-21 14:50:57 +0200

    Add set1/challenge3

--- set1.go
+++ set1.go
@@ -3,34 +3,96 @@ package matasano
 import (
 	"encoding/base64"
 	"encoding/hex"
+	"io/ioutil"
 	"log"
+	"unicode/utf8"
 )
 
 /* challenge 1 */
 func HexToBase64(s string) string {
+
 	r, err := hex.DecodeString(s)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Printf("%s\n", r)
+	log.Printf("set1/c1\t%s\n", r)
 	return base64.StdEncoding.EncodeToString(r)
 }
 
 /* challenge 2 */
-func FixedXor(s1, s2 string) string {
+func Xor(s1, s2 []byte) ([]byte, error) {
 
 	if len(s1) != len(s2) {
-		log.Fatal("Different lengths strings\n")
+		log.Fatal("Different lengths buffers\n")
 	}
 
-	r := make([]byte, len(s1))
+	res := make([]byte, len(s1))
 	for i := 0; i < len(s1); i++ {
-		r[i] = s1[i] ^ s2[i]
+		res[i] = s1[i] ^ s2[i]
 	}
 
-	log.Printf("%s\n", string(r))
-	return string(r)
+	return res, nil
 }
 
-/* challenge 2 */
+/* challenge 3 */
+func SingleByteXor(in []byte, key byte) ([]byte, error) {
+
+	var r []byte = make([]byte, len(in))
+	for i := 0; i < len(r); i++ {
+		r[i] = in[i] ^ key
+	}
+
+	return r, nil
+}
+
+/* the computer cant really tell when a word is an english one, so
+ * we have to sort of train it to do so by taking real world examples
+ * (books will do it) and give him a metric by which it can tell a word from
+ * a non word
+ */
+func AnalyzeCorpus(text string) map[rune]float64 {
+
+	var freq map[rune]float64 = make(map[rune]float64)
+	total := utf8.RuneCountInString(text)
+
+	for _, c := range text {
+		freq[c] += 1
+	}
+
+	/* normalize frequencies */
+	for c := range freq {
+		freq[c] = freq[c] / float64(total)
+	}
+	return freq
+}
+
+/* Count the frequency of each letter and calculate the score
+ * as a weighted sum. naturally, non english words will score lower due to
+ * rare characters being more frequent (lower weigth)
+ */
+func ScoreEnglish(text string, freq map[rune]float64) float64 {
+
+	var score float64
+	var t map[rune]float64 = make(map[rune]float64)
+
+	for _, c := range text {
+		t[c] += 1
+	}
+
+	for c, _ := range t {
+		score += t[c] * freq[c]
+	}
+
+	return score
+}
+
+/* helper function */
+func LoadCorpus(filename string) (string, error) {
+
+	text, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatal("Error opening file")
+	}
+	return string(text), nil
+}
--- set1_test.go
+++ set1_test.go
@@ -1,10 +1,11 @@
 package matasano
 
 import (
+	"encoding/hex"
 	"testing"
 )
 
-func TestHexToBase64(t *testing.T) {
+func TestProblem1(t *testing.T) {
 	test := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 	expected := "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
 
@@ -13,12 +14,52 @@ func TestHexToBase64(t *testing.T) {
 	}
 }
 
-func TestFixedXor(t *testing.T) {
-	s1 := "1c0111001f010100061a024b53535009181c"
-	s2 := "686974207468652062756c6c277320657965"
+func TestProblem2(t *testing.T) {
+	s1 := []byte("1c0111001f010100061a024b53535009181c")
+	s2 := []byte("686974207468652062756c6c277320657965")
 	expected := "746865206b696420646f6e277420706c6179"
 
-	if r := FixedXor(s1, s2); expected != r {
+	r, err := Xor(s1, s2)
+	if err != nil {
 		t.Failed()
 	}
+
+	for i := 0; i < len(s1); i++ {
+		if expected[i] != r[i] || err != nil {
+			t.Failed()
+		}
+	}
+}
+
+func TestProblem3(t *testing.T) {
+	test := "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+
+	var r string
+	var best_score float64
+
+	in, err := hex.DecodeString(test)
+	if err != nil {
+		t.Failed()
+	}
+
+	data, err := LoadCorpus("_testdata/aliceinwonderland.txt")
+	if err != nil {
+		t.Failed()
+	}
+
+	freq := AnalyzeCorpus(data)
+	for k := 0; k < 256; k++ {
+
+		hex, err := SingleByteXor(in, byte(k))
+		if err != nil {
+			t.Failed()
+		}
+
+		tmp := ScoreEnglish(string(hex), freq)
+		if tmp > best_score {
+			r = string(hex)
+			best_score = tmp
+		}
+	}
+	t.Logf("%s", r)
 }
