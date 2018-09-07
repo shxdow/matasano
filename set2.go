abd837f35c81cbf6f5b06017d9602fc07a8b7a49
shxdow <dysonfrost@protonmail.ch>
2018-09-07 22:00:19 +0200

    Fix AES encryption in ECB

--- set2.go
+++ set2.go
@@ -1,14 +1,18 @@
 package matasano
 
 import (
+	"crypto/aes"
 	"errors"
 	"fmt"
+	"log"
 )
 
+// PadPKCS7 pads an arbitrary length string to a certain boundary
+// specified by size
 func PadPKCS7(in []byte, size int) ([]byte, error) {
 
 	if len(in) > size {
-		return nil, errors.New(fmt.Sprintf("Size %d is lower than input length %d", size, len(in)))
+		return nil, errors.New(fmt.Sprintf("size %d is lower than input length %d", size, len(in)))
 	}
 
 	if len(in) == size {
@@ -22,4 +26,30 @@ func PadPKCS7(in []byte, size int) ([]byte, error) {
 	}
 
 	return in, nil
+}
+func AESEncryptECB(data, key []byte) ([]byte, error) {
+
+	var pad int
+	ciphertext := make([]byte, len(data))
+	size := len(key)
+
+	blocks, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	pad = (len(data)/blocks.BlockSize() + 1) * blocks.BlockSize()
+
+	if len(data)%blocks.BlockSize() != 0.0 {
+		data, err = PadPKCS7(data, pad)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	for i := 0; i < len(ciphertext)-size; i += size {
+		blocks.Encrypt(ciphertext[i:i+size], data[i:i+size])
+	}
+
+	return ciphertext, nil
 }
--- set2_test.go
+++ set2_test.go
@@ -18,3 +18,29 @@ func TestProblem9(t *testing.T) {
 		t.Logf("%+v", out)
 	}
 }
+
+func TestAES(t *testing.T) {
+
+	data := []byte("Hello world")
+	key := []byte("YELLOW SUBMARINE")
+
+	// Encrypt the test string
+	enc, err := AESEncryptECB(data, key)
+	if err != nil {
+		t.Log(err)
+		t.FailNow()
+	}
+
+	// Decrypt the test string
+	plain, err := AESDecryptECB(enc, key)
+	if err != nil {
+		t.Log(err)
+		t.FailNow()
+	}
+
+	if !bytes.Equal(plain, enc) {
+		t.Logf("%+v", plain)
+		t.Logf("%+v", enc)
+		t.FailNow()
+	}
+}
