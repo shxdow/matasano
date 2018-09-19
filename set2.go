626e00306c151f58896f7e145bd44b024e5fbadf
shxdow <dysonfrost@protonmail.ch>
2018-09-19 17:23:38 +0200

    Fix PKCS#7 padding
    There were some errors in it due to some misunderstandings on my part

--- set2.go
+++ set2.go
@@ -3,53 +3,169 @@ package matasano
 import (
 	"crypto/aes"
 	"errors"
-	"fmt"
 	"log"
 )
 
-// PadPKCS7 pads an arbitrary length string to a certain boundary
-// specified by size
-func PadPKCS7(in []byte, size int) ([]byte, error) {
+// PadLenPKCS7 returns the length of the padding applied
+// by PadPKCS7(). If no padding is detected 0 is returned
+func PadLenPKCS7(in []byte, block_size int) (int, error) {
 
-	if len(in) > size {
-		return nil, errors.New(fmt.Sprintf("size %d is lower than input length %d", size, len(in)))
+	if len(in)%block_size != 0 {
+		return 0, nil
 	}
 
-	if len(in) == size {
-		return in, nil
+	// Pick the last byte, read its value N and
+	// verify that the last N values are all equal
+	last := int(in[len(in)-1])
+
+	for i := 0; i < last; i++ {
+		if in[len(in)-i-1] != in[len(in)-1] {
+			return 0, errors.New("incorrect padding, block may be corrupted")
+		}
+	}
+	return last, nil
+}
+
+// PadPKCS7 pads an arbitrary length string to any block size
+// from 1 to 255
+func PadPKCS7(in []byte, block_size int) []byte {
+
+	if len(in)%block_size == 0 {
+		// If no padding is detected, proceed and
+		// apply it now
+		if l, err := PadLenPKCS7(in, block_size); l == 0 && err != nil {
+			// In order to determine unambiguosly whether
+			// the last byte of the last plain text block
+			// was introduced via padding or not, an extra
+			// block is added. This block is made of the
+			// byte used for padding
+			for i := 0; i < block_size; i++ {
+				in = append(in, byte(16))
+			}
+		}
+		return in
 	}
 
-	pad := size - len(in)
+	pad := block_size - (len(in) % block_size)
 
+	// Complete the last block
 	for i := 0; i < pad; i++ {
 		in = append(in, byte(pad))
 	}
 
-	return in, nil
+	return in
 }
+
 func AESEncryptECB(data, key []byte) ([]byte, error) {
 
-	var pad int
-	ciphertext := make([]byte, len(data))
-	size := len(key)
+	var ciphertext []byte
+	block_size := 16
 
 	blocks, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
 	}
 
-	pad = (len(data)/blocks.BlockSize() + 1) * blocks.BlockSize()
+	data = PadPKCS7(data, block_size)
 
-	if len(data)%blocks.BlockSize() != 0.0 {
-		data, err = PadPKCS7(data, pad)
-		if err != nil {
-			return nil, err
-		}
+	ciphertext = make([]byte, len(data))
+
+	if err != nil {
+		return nil, err
 	}
 
-	for i := 0; i < len(ciphertext)-size; i += size {
-		blocks.Encrypt(ciphertext[i:i+size], data[i:i+size])
+	for i := 0; i <= len(ciphertext)-block_size; i += block_size {
+		blocks.Encrypt(ciphertext[i:i+block_size], data[i:i+block_size])
 	}
 
 	return ciphertext, nil
+}
+
+func AESEncryptCBC(in, key, iv []byte) ([]byte, error) {
+
+	var block_size int = 16
+	var cipher []byte
+
+	data := PadPKCS7(in, block_size)
+
+	// Xor the first block with the IV
+	enc, err := Xor(data[:block_size], iv)
+	if err != nil {
+		return nil, err
+	}
+
+	cipher, err = AESEncryptECB(enc, key)
+	if err != nil {
+		return nil, err
+	}
+
+	// log.Printf("%+v", cipher)
+
+	// Loop over all but the first block
+	for i := block_size; i <= len(data)-block_size; i += block_size {
+
+		prev := cipher[i-block_size : i]
+		block := data[i : i+block_size]
+
+		// Xor the current block against the one previously encrypted
+		x, err := Xor(block, prev)
+		if err != nil {
+			return nil, err
+		}
+
+		// Proceed with the core encryption which is more or less
+		// AES in ECB
+		b, err := AESEncryptECB(x, key)
+		if err != nil {
+			return nil, err
+		}
+		cipher = append(cipher, b...)
+		// log.Printf("%+v", cipher)
+		// log.Printf("%s", cipher[i:i+block_size])
+		// log.Printf("%+v", cipher[i:i+block_size])
+	}
+
+	return cipher, nil
+}
+
+func AESDecryptCBC(cipher, key, iv []byte) ([]byte, error) {
+
+	var block_size int = 16
+
+	b, err := AESDecryptECB(cipher[:block_size], key)
+	if err != nil {
+		return nil, err
+	}
+
+	// Xor the first block with the IV
+	plain, err := Xor(b, iv)
+	if err != nil {
+		return nil, err
+	}
+
+	/* TODO not sure where the problem is... */
+
+	// Loop over all but the first block
+	for i := block_size; i <= len(cipher)-block_size; i += block_size {
+
+		prev := cipher[i-block_size : i]
+		block := cipher[i : i+block_size]
+
+		// Proceed with the core encryption on the current block
+		b, err := AESDecryptECB(block, key)
+		if err != nil {
+			return nil, err
+		}
+
+		// Xor the current block against the previous cipher
+		p, err := Xor(b, prev)
+		if err != nil {
+			return nil, err
+		}
+
+		log.Printf("%s", p)
+		plain = append(plain, p...)
+	}
+
+	return plain, nil
 }
--- set2_test.go
+++ set2_test.go
@@ -5,27 +5,46 @@ import (
 	"testing"
 )
 
+func TestPadLenPKCS7(t *testing.T) {
+	in := []byte("YELLOW SUBMARINE\x04\x04\x04\x04")
+	expected := 4
+	block_size := 20
+
+	if got, err := PadLenPKCS7(in, block_size); got != expected && err != nil {
+		t.Logf("want: %+v, got: %+v", expected, got)
+		t.FailNow()
+	}
+
+}
+
 func TestProblem9(t *testing.T) {
 	in := []byte("YELLOW SUBMARINE")
 	expected := []byte("YELLOW SUBMARINE\x04\x04\x04\x04")
 	size := 20
 
-	if out, err := PadPKCS7(in, size); !bytes.Equal(out, expected) || err != nil {
+	out := PadPKCS7(in, size)
+	l, err := PadLenPKCS7(in, size)
+	if err != nil {
 		t.Log(err)
 		t.FailNow()
+	}
+
+	if !bytes.Equal(out[:len(out)-l], expected) {
+		t.Logf("%+v", expected)
+		t.FailNow()
 	} else {
-		t.Logf("%+v", string(out))
-		t.Logf("%+v", out)
+		t.Logf("%+v", out[:len(out)-l])
+		t.Logf("%+v", expected)
 	}
 }
 
 func TestAES(t *testing.T) {
 
-	data := []byte("Hello world")
+	test := []byte("Hello world")
 	key := []byte("YELLOW SUBMARINE")
 
 	// Encrypt the test string
-	enc, err := AESEncryptECB(data, key)
+	enc, err := AESEncryptECB(test, key)
 	if err != nil {
 		t.Log(err)
 		t.FailNow()
@@ -38,9 +57,15 @@ func TestAES(t *testing.T) {
 		t.FailNow()
 	}
 
-	if !bytes.Equal(plain, enc) {
+	if !bytes.Equal(plain[:len(test)], test) {
 		t.Logf("%+v", plain)
-		t.Logf("%+v", enc)
+		t.Logf("%+v", test)
+		t.Logf("p: %s", plain)
+		t.Logf("e: %s", test)
+		t.FailNow()
+	}
+}
+
 		t.FailNow()
 	}
 }
